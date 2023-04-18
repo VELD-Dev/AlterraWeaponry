2ab33588591554799645af1315c69053db10a308
V E L Δ <36549174+VELD-Dev@users.noreply.github.com>
2023-04-19 01:50:05 +0200

    "Fixed" PRAWN disappearing when removing the shock upgrade

--- patches/Vehicle_OnUpgradeModuleChange_Patch.cs
+++ patches/Vehicle_OnUpgradeModuleChange_Patch.cs
@@ -10,16 +10,16 @@ internal class Vehicle_OnUpgradeModuleChange_Patch
         {
             if (!added)
             {
-                __instance.gameObject.TryGetComponent<PrawnSelfDefenseModule.ShockFunctionality>(out PrawnSelfDefenseModule.ShockFunctionality defenseMono);
-                defenseMono.gameObject.SetActive(false);
+                //__instance.gameObject.TryGetComponent<PrawnSelfDefenseModule.ShockFunctionality>(out PrawnSelfDefenseModule.ShockFunctionality defenseMono);
+                //defenseMono.gameObject.SetActive(false);
                 AlterraWeaponry.logger.LogInfo("Module Self Defense for Prawn unloaded.");
                 Subtitles.Add("<color=yellow>PRAWN:</color> Self defense module for PRAWN unarmed. Caution advised.");
                 return;
             }
             AlterraWeaponry.logger.LogInfo("Module Self Defense for Prawn loaded in.");
             __instance.gameObject.EnsureComponent<PrawnSelfDefenseModule.ShockFunctionality>();
-            __instance.gameObject.TryGetComponent<PrawnSelfDefenseModule.ShockFunctionality>(out PrawnSelfDefenseModule.ShockFunctionality defenseMonobehaviour);
-            defenseMonobehaviour.gameObject.SetActive(true);
+            //__instance.gameObject.TryGetComponent<PrawnSelfDefenseModule.ShockFunctionality>(out PrawnSelfDefenseModule.ShockFunctionality defenseMonobehaviour);
+            //defenseMonobehaviour.gameObject.SetActive(true);
             Subtitles.Add("<color=yellow>PRAWN:</color> Self defense module for PRAWN loaded and ready.");
         }
     }
